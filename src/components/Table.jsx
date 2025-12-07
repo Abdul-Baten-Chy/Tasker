e66d264b4a9b17e04346798c6da1afaac03ca871
Abdull Baten <abdulbatinchy@gmail.com>
2025-12-08 00:32:39 +0600

    code refactor

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,14 +1,45 @@
-import TableContainer from "./TableContainer";
-
+import TableRow from "./TableRow.jsx";
 function Table({ tasks, onEdit, onDelete, onFav }) {
   return (
     <div class="overflow-auto">
-      <TableContainer
-        tasks={tasks}
-        onEdit={onEdit}
-        onDelete={onDelete}
-        onFav={onFav}
-      />
+      <table class="table-fixed overflow-auto xl:w-full">
+        <thead>
+          <tr>
+            <th class="p-4 pb-8 text-sm font-semibold capitalize w-12"></th>
+            <th class="p-4 pb-8 text-sm font-semibold capitalize w-[300px]">
+              {" "}
+              Title{" "}
+            </th>
+            <th class="p-4 pb-8 text-sm font-semibold capitalize w-full">
+              {" "}
+              Description{" "}
+            </th>
+            <th class="p-4 pb-8 text-sm font-semibold capitalize md:w-[350px]">
+              {" "}
+              Tags{" "}
+            </th>
+            <th class="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">
+              {" "}
+              Priority{" "}
+            </th>
+            <th class="p-4 pb-8 text-sm font-semibold capitalize md:w-[100px]">
+              {" "}
+              Options{" "}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <TableRow
+              key={task.id}
+              task={task}
+              onEdit={onEdit}
+              onDelete={onDelete}
+              onFav={onFav}
+            />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
